0d9788a158b2fb1f7cbfa8f30f460e91c26c3599
hiruthikj <hiruthik27@gmail.com>
2020-12-27 18:23:27 +0530

    login and home

--- book_management_system/online_books/urls.py
+++ book_management_system/online_books/urls.py
@@ -1,25 +1,12 @@
 from django.conf.urls import url,include
 from django.urls import path
 from . import views
-# from .forms import StudentLoginForm
 from django.contrib.auth.views import *
     #LoginView,LogoutView,PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 
 app_name = 'online_books'
 
 urlpatterns = [
-
-    # path('', LoginView.as_view(template_name='stud_app/login.html',authentication_form=StudentLoginForm), name="login"),
-    # url('login/',  views.StudentView.as_view(), name="login"),
-
-    # path('', views.blank_page),
-    # path('login/', views.login_view, name="login"),
-    path('login/',  login, name="login"),
-    path('logout/', LogoutView.as_view(template_name='online_books/logout.html'), name="logout"),
-    # path('<username>/home/', views.home_view, name="home"),
-    # path('<username>/courses/', views.courses_view, name="courses"),
-    # path('<username>/exams/', views.exam_list_view, name="exams"),
-    # path('<username>/exams/<int:exam_id>', views.exam_view, name="exams"),
-    # path('<username>/scores/', views.scores_view, name="scores"),
+    path('home/', views.home_view, name='home'),
 
 ]
--- book_management_system/online_books/views.py
+++ book_management_system/online_books/views.py
@@ -1,3 +1,13 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+@login_required()
+def home_view(request):
+    books = Book.objects.all()
+    return render(request, 'home.html', context={
+        'books' : books,
+        })
